fcb036102aaa3de9c4724b2cdfc1551289d562e9
Matthieu Gouel <matthieu.gouel@protonmail.com>
2021-05-06 14:06:16 +0200

    fix token url

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -7,7 +7,7 @@ class AuthService {
         params.append('username', user.username);
         params.append('password', user.password);
         return axios
-            .post(process.env.VUE_APP_BACKEND_URL + '/profile/token/', params)
+            .post(process.env.VUE_APP_BACKEND_URL + '/profile/token', params)
             .then(response => {
                 if (response.data.access_token) {
                     localStorage.setItem('user', JSON.stringify(response.data));
