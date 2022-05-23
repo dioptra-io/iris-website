1265d9caf7bb367fbb310385bdd86ec76595d7e9
Maxime Mouchet <max@maxmouchet.com>
2022-05-23 10:58:28 +0200

    show file size in measurement results view

--- src/components/MeasurementResults.vue
+++ src/components/MeasurementResults.vue
@@ -1,24 +1,24 @@
 <template>
   <div
-    style="display: inline-table"
-    v-for="result in results"
-    :key="result.name"
+      style="display: inline-table"
+      v-for="result in results"
+      :key="result.name"
   >
     <a
-      v-if="result.format !== undefined"
-      class="uk-button uk-button-primary"
-      download
-      :href="result.url"
+        v-if="result.format !== undefined"
+        class="uk-button uk-button-primary"
+        download
+        :href="result.url"
     >
-      {{ result.format }}
+      {{ result.format }} ({{ result.size }})
     </a>
     <span>&nbsp;</span>
   </div>
 
   <router-link
-    style="display: inline-table"
-    class="uk-button uk-button-primary"
-    :to="{
+      style="display: inline-table"
+      class="uk-button uk-button-primary"
+      :to="{
       name: 'SQL',
       params: {
         series: series,
@@ -26,12 +26,14 @@
         agentUUID: agentUUID,
       },
     }"
-    >SQL</router-link
+  >SQL
+  </router-link
   >
 </template>
 
 <script>
 import S3Service from "../services/s3.service";
+import filesize from "filesize";
 
 export default {
   name: "MeasurementResults",
@@ -48,17 +50,18 @@ export default {
   mounted() {
     S3Service.getFiles(this.measurementUUID, this.agentUUID).then((files) => {
       this.results = files;
-
       for (let index = 0; index < this.results.length; index++) {
-        S3Service.getFileURL("public-exports", this.results[index].name).then(
-          (presignedUrl) => {
-            this.results[index].url = presignedUrl;
-            // Required name convention: measurementUUID__agentUUID__format
-            this.results[index].format = this.results[index].name
+        Promise.all([
+          S3Service.getFileURL("public-exports", this.results[index].name),
+          S3Service.getFileStat("public-exports", this.results[index].name)
+        ]).then(result => {
+          this.results[index].url = result[0];
+          // Required name convention: measurementUUID__agentUUID__format
+          this.results[index].format = this.results[index].name
               .split(".")[0]
               .split("__")[2];
-          }
-        );
+          this.results[index].size = filesize(result[1].size);
+        });
       }
     });
   },
--- src/services/s3.service.js
+++ src/services/s3.service.js
@@ -4,25 +4,26 @@ var Minio = require('minio')
 
 
 class S3Service {
+    getClient(credentials) {
+        const s3_endpoint = new URL(credentials.s3.endpoint_url).hostname;
+        return new Minio.Client({
+            endPoint: s3_endpoint,
+            port: 443,
+            useSSL: true,
+            accessKey: credentials.s3.aws_access_key_id,
+            secretKey: credentials.s3.aws_secret_access_key,
+            sessionToken: credentials.s3.aws_session_token,
+        });
+    }
+
     getFiles(measurementUUID, agentUUID) {
         return axios.get(
             process.env.VUE_APP_BACKEND_URL + '/users/me/services',
         ).then(response => {
-            var credentials = response.data;
-            var s3_endpoint = new URL(credentials.s3.endpoint_url).hostname;
-            var s3Client = new Minio.Client({
-                endPoint: s3_endpoint,
-                port: 443,
-                useSSL: true,
-                accessKey: credentials.s3.aws_access_key_id,
-                secretKey: credentials.s3.aws_secret_access_key,
-                sessionToken: credentials.s3.aws_session_token,
-            });
-
+            const s3Client = this.getClient(response.data);
             var objectsStream = s3Client.listObjectsV2(
                 'public-exports', `${measurementUUID}__${agentUUID}`, false, ''
             );
-
             let ret = []
             return new Promise(function (resolve, reject) {
                 objectsStream.on('data', function (obj) {
@@ -42,18 +43,17 @@ class S3Service {
         return axios.get(
             process.env.VUE_APP_BACKEND_URL + '/users/me/services',
         ).then(response => {
-            var credentials = response.data;
-            var s3_endpoint = new URL(credentials.s3.endpoint_url).hostname;
-            var s3Client = new Minio.Client({
-                endPoint: s3_endpoint,
-                port: 443,
-                useSSL: true,
-                accessKey: credentials.s3.aws_access_key_id,
-                secretKey: credentials.s3.aws_secret_access_key,
-                sessionToken: credentials.s3.aws_session_token,
-            });
-
+            const s3Client = this.getClient(response.data);
             return s3Client.presignedGetObject(bucket, object, 60 * 60, {});
+        });
+    }
+
+    getFileStat(bucket, object) {
+        return axios.get(
+            process.env.VUE_APP_BACKEND_URL + '/users/me/services',
+        ).then(response => {
+            const s3Client = this.getClient(response.data);
+            return s3Client.statObject(bucket, object);
         });
     }
 }
